ffedb53e90632d09ff93c83e7603865dc1498962
Slashaershov <127024236+Slashaershov@users.noreply.github.com>
2025-02-21 05:35:05 +0300

    add ex 1 and ex 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,29 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        //ex 1
+        byte age =29;
+        System.out.print("Если возраст человека равен " + age + ", то он");
+        if (age >= 18)
+        {
+            System.out.println(" совершеннолетний.");
+        }
+        else
+        {
+            System.out.println(" не достиг совершеннолетия, нужно немного подождать");
+        }
+
+        //ex2
+        byte t = 52;
+        if (t<5)
+        {
+            System.out.println("На улице холодно, нужно надеть шапку");
+        }
+        else
+        {
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        }
+
+        //ex3
     }
 }
