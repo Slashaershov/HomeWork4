9d425892040bbc77776c911593c15ec47bf26184
Slashaershov <127024236+Slashaershov@users.noreply.github.com>
2025-02-21 05:59:19 +0300

    add ex 3, ex 4,

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,53 @@ public class Main
     public static void main(String[] args)
     {
         //ex 1
-        byte age =29;
+        byte age = 29;
         System.out.print("Если возраст человека равен " + age + ", то он");
         if (age >= 18)
         {
             System.out.println(" совершеннолетний.");
-        }
-        else
+        } else
         {
             System.out.println(" не достиг совершеннолетия, нужно немного подождать");
         }
 
         //ex2
         byte t = 52;
-        if (t<5)
+        if (t < 5)
         {
             System.out.println("На улице холодно, нужно надеть шапку");
-        }
-        else
+        } else
         {
             System.out.println("Сегодня тепло, можно идти без шапки");
         }
 
         //ex3
+        int speed = 52;
+        System.out.print("Если скорость " + speed + ", то ");
+        if (speed > 60)
+        {
+            System.out.println("придется заплатить штраф");
+        } else
+        {
+            System.out.println("можно ездить спокойно");
+        }
+
+        //ex4
+        age = 52;
+        System.out.print("Если возраст человека равен " + age + ", то ему нужно ходить ");
+
+        if (2 <= age && age <= 6)
+        {
+            System.out.println("в детский сад");
+        } else if (7 <= age && age <= 17)
+        {
+            System.out.println("в школу");
+        } else if (18 <= age && age <= 24)
+        {
+            System.out.println("в университет");
+        } else if (age > 25)
+        {
+            System.out.println("на работу");
+        }
     }
 }
